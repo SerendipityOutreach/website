d722cb68365667f8151e30472cdc96e2d4291da2
kyleng <ng.kyle101@gmail.com>
2021-02-27 22:14:52 -0800

    Uss v-bind object instead of a bunch of strings

--- serendpity/src/components/FormattedContent.vue
+++ serendpity/src/components/FormattedContent.vue
@@ -1,35 +1,30 @@
 <template>
-<div>
-  <div
-      v-for="description in content_list"
-      :key="description.title"
-
-  >
-    <p>{{description.type}}</p>
-    <p>{{description}}</p>
-    <PicLeft
-        v-if="description.type === `LeftPic`"
-        :content="description"
-        style="margin-bottom: 10px"
-    ></PicLeft>
-    <PicRight
-        v-if="description.type === `RightPic`"
-        :title="description.title"
-        :image_input="description.image"
-        :color="description.color"
-        :description="description.description"
-        style="margin-bottom: 10px"
-    ></PicRight>
+  <div>
+    <div
+        v-for="description in content_list"
+        :key="description.title"
+    >
+      <PicLeft
+          v-if="description.type === `LeftPic`"
+          :content="description"
+          style="margin-bottom: 10px"
+      ></PicLeft>
+      <PicRight
+          v-if="description.type === `RightPic`"
+          :content="description"
+          style="margin-bottom: 10px"
+      ></PicRight>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
 import PicLeft from "@/components/PicLeft";
 import PicRight from "@/components/PicRight";
+
 export default {
   name: "FormattedContent",
-  props:{
+  props: {
     content_list: Array,
     max_height: String,
   },
