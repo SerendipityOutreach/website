36711205d8e39da418ef356d4f6b5f5b2346ead2
kyleng <ng.kyle101@gmail.com>
2021-02-28 21:57:06 -0800

    Interim Serendipity STEM About Page

--- serendpity/src/assets/descriptions/AboutDescription.js
+++ serendpity/src/assets/descriptions/AboutDescription.js
@@ -5,24 +5,24 @@ const WhoAreWe = {
     type: "LeftPic",
     title: "Who are we?",
     image: "FullLogo.png",
-    color: "red lighten-2",
+    color: "red lighten-1",
     description: "Serendipity STEM is a student-led outreach community of and FIRST Tech Challenge teams in the Bay Area.",
 }
 const OurMission = {
     type: "RightPic",
     title: "Our Mission",
     image: "Brain.jpg",
-    color: "red darken-1",
-    description: "Our goal is to provide sustainable STEM opportunities for Special Education students in the form of Robotics and Coding",
+    color: "blue darken-1",
+    description: "Our goal is to provide sustainable STEM opportunities for Special Education students in the form of robotics and coding",
 }
 const OurPassion = {
     type: "TripleView",
     title: "Our Passion",
     image: "BridgeFiltered.jpg",
     image_length: 80, // Change this to make sure you can see the whole picture
-    color: "orange darken-1",
+    color: "red lighten-1",
     descriptions: [
-        "Being part of the various FIRST Tech Challenge, Serendipity STEM help us to understand the societal application of what we learn in our robotics teams",
+        "Being part of the various FIRST Tech Challenge, Serendipity STEM help us understand the societal application of what we learn in our robotics teams",
         "Although Silicon Valley is the heart of technology, Special Education students do not have the same access to STEM opportunities as General Education students",
         "By spreading not just coding--but a way of approaching life," +
         " we can use engineering as a catalyst to bridge the gap between the STEM and Special Education community",
--- serendpity/src/components/MultiPanel.vue
+++ serendpity/src/components/MultiPanel.vue
@@ -12,7 +12,7 @@
               :style="[{paddingTop: content.image_length/2 + `px`, paddingBottom: content.image_length/2 + `px`}]"
           >
             <v-col v-for="i in 3" :key="i" cols="4">
-              <v-card :color="content.color" class="rounded-xl">
+              <v-card :color="content.color" class="rounded-lg">
                 <v-card-title v-if="i===2" class="text-h3 ml-7" v-text="content.title"></v-card-title>
               </v-card>
             </v-col>
@@ -24,7 +24,7 @@
 <!--            cols length are dynamically resized so that they are always centered -->
             <v-col v-for="text_block in content.descriptions" :key="text_block" :cols="12/content.descriptions.length">
               <v-card
-                  class="mb-12 rounded-xl"
+                  class="mb-12 rounded-lg"
                   :color="content.color"
               >
                 <v-card-subtitle class="headline text-h6 white--text" v-text="text_block"></v-card-subtitle>
