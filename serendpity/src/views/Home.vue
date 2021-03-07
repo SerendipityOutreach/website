f3c1427d9677bdda98e78d1c76d0b4bcada8abc6
kyleng <ng.kyle101@gmail.com>
2021-03-07 15:13:57 -0800

    Timelines are not supposed to be Navbars, better to stick with the traditional nav bar

--- serendpity/src/views/Home.vue
+++ serendpity/src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <v-container>
-    <About></About>
-  </v-container>
+  <div class="pr-5">
+    <v-row>
+      <v-col cols="2">
+        <Nav></Nav>
+      </v-col>
+      <v-col cols="10">
+<!--         put main content here    -->
+        <About v-if="false"></About>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
 import About from "@/views/sub_views/About"
 import {descriptions} from "@/assets/descriptions/AboutDescription";
+import Nav from "@/views/Nav";
 
 export default {
   name: 'Home',
 
   components: {
-    About
+    About,
+    Nav,
   },
 
   props: {},
--- serendpity/src/views/Nav.vue
+++ serendpity/src/views/Nav.vue
@@ -1,20 +1,36 @@
 <template>
   <div>
-    <v-timeline v-for="section in NavDescriptions" :key="section">
-      <div>
-        <v-timeline-item slot="opposite" :color="section.color"> {{ section.section_name }} </v-timeline-item>
-      </div>
+    <v-row>
+      <v-col cols="8">
+        <v-timeline v-for="section in NavDescriptions" :key="section.color">
+          <div>
+            <v-timeline-item>
+              {{ section.section_name }}
+            <v-btn
+                fab
+                x-small
+                :color="section.color"
+                @click="broadcastScrollSection(section.section_name)">
 
-      <div v-for="sub_section in section.content" :key="sub_section">
-        <v-timeline-item
-            :color="section.color + ` lighten-2`"
-            small
-        >
-          {{ sub_section.title }}
-        </v-timeline-item>
-      </div>
 
-    </v-timeline>
+
+            </v-btn>
+            </v-timeline-item>
+          </div>
+
+          <div v-for="sub_section in section.content" :key="sub_section.title">
+            <v-timeline-item
+                :color="section.color + ` lighten-2`"
+                small
+            >
+              {{ sub_section.title }}
+            </v-timeline-item>
+          </div>
+
+        </v-timeline>
+      </v-col>
+      <v-col cols="5"></v-col>
+    </v-row>
   </div>
 </template>
 
@@ -26,6 +42,11 @@ export default {
   data() {
     return {
       NavDescriptions
+    }
+  },
+  methods: {
+    broadcastScrollSection(scrollSection){
+      console.log("Broadcasting scroll section " + scrollSection)
     }
   }
 }
