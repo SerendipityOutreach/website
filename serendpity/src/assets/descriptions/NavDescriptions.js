b46b09b590a90410692de1cce7553bc93d64144a
kyleng <ng.kyle101@gmail.com>
2021-03-07 14:25:44 -0800

    Added basic frontend info

---
+++ serendpity/src/assets/descriptions/NavDescriptions.js
@@ -0,0 +1,19 @@
+import {AboutContent} from "@/assets/descriptions/AboutDescription";
+
+const NavDescriptions = [
+    {
+        // About Section
+        section_name: "About Us",
+        content: AboutContent,
+        // just put a general color, the nav.vue component will take care of shading
+        color: "red",
+    },
+    {
+        // Our Work Section with dummy data for now
+        section_name: "Our Work",
+        content: AboutContent,
+        color: "blue",
+    }
+]
+
+export {NavDescriptions}
---
+++ serendpity/src/views/Nav.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <v-timeline v-for="section in NavDescriptions" :key="section">
+      <div>
+        <v-timeline-item slot="opposite" :color="section.color"> {{ section.section_name }} </v-timeline-item>
+      </div>
+
+      <div v-for="sub_section in section.content" :key="sub_section">
+        <v-timeline-item
+            :color="section.color + ` lighten-2`"
+            small
+        >
+          {{ sub_section.title }}
+        </v-timeline-item>
+      </div>
+
+    </v-timeline>
+  </div>
+</template>
+
+<script>
+import {NavDescriptions} from "@/assets/descriptions/NavDescriptions";
+
+export default {
+  name: "Nav",
+  data() {
+    return {
+      NavDescriptions
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
